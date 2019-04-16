bc355d8b1a8388eef50e894de71acb79fcf760c6
zhoulonghui0529 <1044208076@qq.com>
2019-04-16 16:36:04 +0800

    add readme ffile

---
+++ js/piccycle.js
@@ -0,0 +1,96 @@
+var index = 1,
+	timer;
+init();
+
+function init() {
+	eventBind();
+	autoPlay();
+}
+
+function eventBind() {
+	/*点的点击事件*/
+	var dots = document.getElementsByClassName("dot");
+	var len = dots.length;
+	for(var i = 0; i < len; i++) {
+		(function(j) {
+			dots[j].onclick = function(e) {
+				var ind = parseInt(dots[j].getAttribute("index"));
+				animation((index - ind) * (-935)); /*显示点击的图片*/
+				index = ind;
+				dotActive();
+			}
+		})(i)
+	}
+	/*容器的hover事件*/
+	var con = document.getElementsByClassName("container")[0];
+	/*鼠标移动到容器上时，停止制动滑动，离开时继续滚动*/
+	con.onmouseover = function(e) {
+		stopAutoPlay();
+	}
+	con.onmouseout = function(e) {
+		autoPlay();
+	}
+	/*箭头事件的绑定*/
+	var pre = document.getElementsByClassName("pre")[0];
+	var next = document.getElementsByClassName("next")[0];
+	pre.onclick = function(e) {
+		dotIndex(false);
+		animation(935);
+	}
+	next.onclick = function(e) {
+		dotIndex(true);
+		animation(-935);
+	}
+}
+
+function autoPlay() {
+	timer = setInterval(function() {
+		animation(-935);
+		dotIndex(true);
+	}, 2000)
+}
+
+function stopAutoPlay() {
+	clearInterval(timer);
+}
+
+function dotIndex(add) {
+	if(add) {
+		index++;
+	} else {
+		index--;
+	}
+	if(index > 5) {
+		index = 1;
+	}
+	if(index < 1) {
+		index = 5;
+	}
+	dotActive();
+}
+
+function dotActive() {
+	var dots = document.getElementsByClassName("dot");
+	var len = dots.length;
+	for(var i = 0; i < len; i++) {
+		dots[i].className = "dot";
+	}
+	for(var i = 0; i < len; i++) {
+		/*此处可以不用parseInt，当不用全等时*/
+		if(index === parseInt(dots[i].getAttribute("index"))) {
+			dots[i].className = "dot active";
+		}
+	}
+}
+
+function animation(offset) {
+	var lists = document.getElementsByClassName("list")[0];
+	var left = parseInt(lists.style.left.slice(0, lists.style.left.indexOf("p"))) + offset;
+	if(left < -4675) {
+		lists.style.left = "-935px";
+	} else if(left > -935) {
+		lists.style.left = "-4675px";
+	} else {
+		lists.style.left = left + "px";
+	}
+}
